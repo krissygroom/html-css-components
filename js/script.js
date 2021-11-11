5a4b19a93e590fe48b42ddf06326827e7b768895
krissy <krissydgroom@gmail.com>
2021-11-10 16:21:31 -0800

    Edited js to show up arrow icon when accordian is open

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var arrow = document.querySelectorAll(".icon");
+var arrow = document.querySelectorAll(".open-icon");
 var clicked = false;
 
 arrow.forEach(arrow => {
@@ -7,11 +7,18 @@ arrow.forEach(arrow => {
         if (!clicked) {
             clicked = true;
             arrow.parentNode.classList.add('open');
-
+            arrow.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
+          </svg>`;
         }
         else {
             clicked = false;
-            arrow.parentNode.classList.remove('open');
+            if (arrow.parentNode.classList.contains('open')) {
+                arrow.parentNode.classList.remove('open');
+                arrow.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+              </svg>`;
+            }
         }
     });
 });
